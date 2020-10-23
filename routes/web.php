c53e473925ee53e3a88e81933c1ee57bb0fe1976
valentin.wyss <valentin.wyss@he-arc.ch>
2020-10-23 11:33:23 +0200

    Create all controllers and display custom view in MapController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MapController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+Route::get('/map', [MapController::class, 'index'])->name('home');
 
 Route::get('/', function () {
     return view('welcome');
