2304c364d12ad21eb35b42a4a5119bbb3fdd6f8b
Nicolas <Nicolas.Laoun@he-arc.ch>
2020-10-23 09:23:28 +0200

    database config

---
+++ database/migrations/2020_10_23_071053_create_conversations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->int('radius');
+            $table->dateTime('timeOfDeath');
+            $table->string('latlng');
+            $table->int('creatorID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
