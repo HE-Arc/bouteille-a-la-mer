9e38e6f2e1b14e13a86c40ae24119143b68abb6a
Nicolas <Nicolas.Laoun@he-arc.ch>
2020-10-30 11:59:31 +0100

    WE CAN EMIT EVENT !!!!!!!!

--- resources/views/testWebSocket.blade.php
+++ resources/views/testWebSocket.blade.php
@@ -7,6 +7,10 @@
 
     <body>
         yooooo
+        <?php
+            echo "test";
+            event(new App\Events\newMessage("yo"))
+         ?>
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
         <script type="text/javascript" src="{{ URL::asset('js/testSocket.js') }}"></script>
     </body>
