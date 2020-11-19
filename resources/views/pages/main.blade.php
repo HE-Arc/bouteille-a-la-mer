c9457415eac744f19621b6f173aff727f16350e4
mathias.marty <mathias.marty@he-arc.ch>
2020-11-19 21:37:18 +0100

    Add the button to drop a bottle

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('content')
-<div id='app'>
+<div id="app">
 	<nav style="margin-top: 0px;" id="status">
 		<div class="nav-wrapper">
 			<p v-if="connected" class="flow-text">
@@ -12,10 +12,13 @@
 		</div>
 	</nav>
 
-	<div id='map'></div>
+	<div id="map"></div>
 	
 	<div class="container">
 			<!-- Page Content goes here -->
+			<a id="drop-btn" class="btn-floating btn-large waves-effect waves-light">
+				<img id="drop-img" src="{{ URL::asset('/img/drop_bottle.png') }}">
+			</a>
 	</div>
 </div>
 
