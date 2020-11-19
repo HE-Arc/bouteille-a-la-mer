29f1d5b446669b3987abf46aadae1030bf59949d
mathias.marty <mathias.marty@he-arc.ch>
2020-11-19 18:51:37 +0100

    Correct ressource not find errors

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="resources/css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="{{ URL::asset('materialize/css/materialize.min.css') }}"  media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -14,18 +14,12 @@
 
     <!--Fav icon-->
     <link rel="shortcut icon" type="image/png" href="{{ URL::asset('/img/logo.png') }}"/>
-
-    <!-- custom css-->
-    <link href="{{ URL::asset('/css/app.css') }}" rel='stylesheet' />
 </head>
 
 <body>
     @yield('content')
 
     <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="resources/js/materialize.min.js"></script>
-
-    <!-- custom js-->
-    <script src="{{ URL::asset('/js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('materialize/js/materialize.min.js') }}"></script>
 </body>
 </html>
