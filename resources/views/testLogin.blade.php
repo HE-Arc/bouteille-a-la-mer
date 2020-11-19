d6fff5fd8d981035dba143d7eceab11f0e458572
Nicolas <Nicolas.Laoun@he-arc.ch>
2020-11-19 18:57:34 +0100

    websockets

---
+++ resources/views/testLogin.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+<form action="/trySignup" method="post">
+    <h1>Inscription</h1>
+    <input type="text">
+    <input type="password">
+    <input type="submit" value="Sign up">
+</form>
+<form action="/tryLogin" method="post">
+    <h1>Login</h1>
+    <input type="text">
+    <input type="password">
+    <input type="submit" value="Login">
+</form>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/socket', function () {
+    return view('testSocket');
+});
+
+
+
+Route::get('/login', [LoginController::class, 'login']);
+Route::post('/tryLogin', [LoginController::class, 'tryLogin']);
+Route::post('/trySignup', [LoginController::class, 'trySignup']);
