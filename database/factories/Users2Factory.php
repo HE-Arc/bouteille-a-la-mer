4c68474b9d7c321d2ac909fa3cda43041b547821
Nicolas <Nicolas.Laoun@he-arc.ch>
2020-11-19 18:57:34 +0100

    websockets

---
+++ database/factories/Users2Factory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Users2;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Users2Factory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Users2::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
